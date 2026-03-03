4d33dfd598d0fba4c9987dc44136c1a4f2034b72
Vijay balaji <balajibaskaran456@gmail.com>
2026-03-03 23:38:09 +0530

    Small update to backend logic

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM eclipse-temurin:21-jdk as builder
-WORKDIR /build
-COPY . .
-RUN ./mvnw clean package -DskipTests
-FROM eclipse-temurin:21-jre
+FROM eclipse-temurin:21-jdk
+
 WORKDIR /app
-COPY --from=builder /build/target/bookings-0.0.1-SNAPSHOT.jar app.jar
-COPY ./src/main/resources/application-prod.properties /app/config/application-prod.properties
-COPY ./.env /app
-ENV SPRING_CONFIG_LOCATION=/app/config/application-prod.properties
-ENV SPRING_PROFILES_ACTIVE=prod
-ENTRYPOINT ["java","-jar","app.jar"]
+
+COPY . .
+
+RUN ./mvnw clean package -DskipTests
+
+EXPOSE 5050
+
+CMD ["java", "-jar", "target/bookings-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/furnize/bookings/configuration/CorsConfig.java
+++ src/main/java/com/furnize/bookings/configuration/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.furnize.bookings.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,11 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer{
 	
 	
-		@Override
+	@Value("${FE_URL}")
+	private String feUrlString1;
+	@Value("http://127.0.0.1:5500")
+	private String feUrString2;
+		
+	@Override
 		public void addCorsMappings(CorsRegistry registry) {
 			
 			registry.addMapping("/**")
-					.allowedOrigins("http://localhost:5500","http://127.0.0.1:5500","http://192.168.0.105:5500")
+					.allowedOrigins(feUrlString1,feUrString2)
 					.allowedMethods("GET","POST","PUT","DELETE")
 					.allowedHeaders("*");
 					
