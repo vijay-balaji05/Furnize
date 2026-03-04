73c0ba4e62a562190c7f474ca345ac9db3393782
Vijay balaji <balajibaskaran456@gmail.com>
2026-03-05 01:01:13 +0530

    Backend Ready for Deployment

--- src/test/java/com/furnize/bookings/BookingsApplicationTests.java
+++ src/test/java/com/furnize/bookings/BookingsApplicationTests.java
@@ -2,12 +2,14 @@ package com.furnize.bookings;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Uses application-test.properties
 class BookingsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Simply checks if Spring context loads
+    }
 }
